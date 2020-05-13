89ba21be24595533e276fa744b8b0e9fb2aaf1ef
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-05-13 14:57:18 +0530

    Update SpeakGoodBye.js

--- Module-4Assignment/SpeakGoodBye.js
+++ Module-4Assignment/SpeakGoodBye.js
@@ -2,7 +2,7 @@
     var byeSpeaker = new Object();
     var speakWord = "Good Bye";
     byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name+" !!");
+        console.log(speakWord + " " + name);
     };
     window.byeSpeaker = byeSpeaker;
 })(window);
