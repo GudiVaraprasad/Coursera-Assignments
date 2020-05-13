e69e40834b5e7245c142a76c3b76bf1d53cf08fb
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-05-13 14:51:55 +0530

    Update script.js

--- Module-4Assignment/script.js
+++ Module-4Assignment/script.js
@@ -1,7 +1,7 @@
 
 
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "Jname1", "Jname2", "Jname3", "Paul", "Frank", "Larry", "Paula", "Laura", "Jname4"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
@@ -19,13 +19,13 @@ Solution of Module 4:
 Expected output:
 
 Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
+Good Bye Jname1
+Good Bye Jname2
+Good Bye Jname3
 Hello Paul
 Hello Frank
 Hello Larry
 Hello Paula
 Hello Laura
-Good Bye Jim
+Good Bye Jname4
 */
